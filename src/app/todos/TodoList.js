f09167172744181ace9a84d667ad18962145f767
Kyler Johnson <kylerjohnsondev@gmail.com>
2024-02-14 21:43:28 -0500

    add todo list cmp, impl server action to create todo

---
+++ src/app/todos/TodoList.js
@@ -0,0 +1,20 @@
+import TodoItem from "./TodoItem";
+
+export default function TodoList({ todos }) {
+  if (todos.length === 0) {
+    return (
+      <div className="empty-todos">
+        <h1>Nothing to do!</h1>
+        <p>Start by adding a new todo.</p>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="todo-container">
+      {todos.map((todo, index) => (
+        <TodoItem key={index} todo={todo} index={index} />
+      ))}
+    </ul>
+  );
+}
--- src/app/todos/page.js
+++ src/app/todos/page.js
@@ -1,25 +1,32 @@
 import { auth } from "@clerk/nextjs";
-import { fetchTodosByUserId } from "@/database/todos";
-import TodoItem from "./TodoItem";
+import { fetchTodosByUserId, createTodo } from "@/database/todos";
+import TodoList from "./TodoList";
+import { revalidatePath } from "next/cache";
 
 export default async function TodosPage() {
   const { userId } = auth();
   const todos = await fetchTodosByUserId(userId);
 
-  if (todos.length === 0) {
-    return (
-      <div className="empty-todos">
-        <h1>Nothing to do!</h1>
-        <p>Start by adding a new todo.</p>
-      </div>
-    );
+  async function addTodo(formData) {
+    "use server";
+    const { userId } = auth();
+    const todoText = formData.get("todoText");
+    await createTodo(userId, todoText);
+    revalidatePath("/todos");
   }
 
   return (
-    <ul className="todo-container">
-      {todos.map((todo, index) => (
-        <TodoItem key={index} todo={todo} index={index} />
-      ))}
-    </ul>
+    <>
+      <form action={addTodo}>
+        <input
+          name="todoText"
+          className="todo-input"
+          type="text"
+          placeholder="Add a new todo"
+        />
+      </form>
+
+      <TodoList todos={todos} />
+    </>
   );
 }
